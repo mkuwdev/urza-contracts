5163692e2bce806ca2e0405db8cc2fccadf1b9ea
[your <alessandrofigo24@gmail.com>
2023-03-19 16:19:08 +0800

    changed to deploy with safe

--- deploy/2_deploy_aaSafe.ts
+++ deploy/2_deploy_aaSafe.ts
@@ -1,6 +1,8 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { Create2Factory } from '../src/Create2Factory';
+import gnosis_safe from '@safe-global/safe-deployments/src/assets/v1.3.0/gnosis_safe.json';
+import proxy_factory from '@safe-global/safe-deployments/src/assets/v1.3.0/proxy_factory.json';
 import { ethers } from 'hardhat';
 
 const deployAASafe: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
@@ -10,23 +12,30 @@ const deployAASafe: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const provider = ethers.provider;
   const from = await provider.getSigner().getAddress();
 
-  const safeSingleton = await deploy('GnosisSafe', {
-    from,
-    args: [],
-    gasLimit: 6e6,
-    deterministicDeployment: true,
-  });
+  // const safeSingleton = await deploy('GnosisSafe', {
+  //   from,
+  //   args: [],
+  //   gasLimit: 6e6,
+  //   deterministicDeployment: true,
+  // });
 
-  console.log('== Safe Address == ', safeSingleton.address);
+  // console.log('== Safe Address == ', safeSingleton.address);
 
-  const proxyFactory = await deploy('GnosisSafeProxyFactory', {
-    from,
-    args: [],
-    gasLimit: 6e6,
-    deterministicDeployment: true,
-  });
+  // const proxyFactory = await deploy('GnosisSafeProxyFactory', {
+  //   from,
+  //   args: [],
+  //   gasLimit: 6e6,
+  //   deterministicDeployment: true,
+  // });
 
-  console.log('== Safe Proxy Factory Address == ', proxyFactory.address);
+  // console.log('== Safe Proxy Factory Address == ', proxyFactory.address);
+
+  // const safeSingleton = await getSafeSingletonDeployment({ network: '5' });
+  const safeSingletonAddress = gnosis_safe.networkAddresses['5'];
+  console.log('== Safe Address == ', safeSingletonAddress);
+  // const proxyFactory = getProxyFactoryDeployment({ network: '5' });
+  const proxyFactoryAddress = proxy_factory.networkAddresses['5'];
+  console.log('== Safe Proxy Factory Address == ', proxyFactoryAddress);
 
   const entrypoint = await deployments.get('EntryPoint');
   const manager = await deploy('EIP4337Manager', {
@@ -40,7 +49,7 @@ const deployAASafe: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
   const accountFactory = await deploy('GnosisSafeAccountFactory', {
     from,
-    args: [proxyFactory.address, safeSingleton.address, manager.address],
+    args: [proxyFactoryAddress, safeSingletonAddress, manager.address],
     gasLimit: 6e6,
     deterministicDeployment: true,
   });
@@ -49,8 +58,8 @@ const deployAASafe: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 };
 
 deployAASafe.tags = [
-  'GnosisSafe',
-  'GnosisSafeProxyFactory',
+  // 'GnosisSafe',
+  // 'GnosisSafeProxyFactory',
   'EIP4337Manager',
   'GnosisSafeAccountFactory',
 ];
